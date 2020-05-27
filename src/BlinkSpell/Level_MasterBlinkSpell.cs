266fd333bfcc37daf37e264fb8e88c9d1182bc9d
extract <decodeish@gmail.com>
2020-05-27 11:47:55 +0200

    Initial commit

---
+++ src/BlinkSpell/Level_MasterBlinkSpell.cs
@@ -0,0 +1,23 @@
+﻿using BS;
+using System;
+using UnityEngine;
+
+namespace BlinkSpell
+{
+    public class Level_MasterBlinkSpell : LevelModule
+    {
+        public override void OnLevelLoaded(LevelDefinition levelDefinition)
+        {
+            initialized = true;
+        }
+        public override void Update(LevelDefinition levelDefinition)
+        {
+            if (!initialized) return;
+            // Add code here.
+        }
+        public override void OnLevelUnloaded(LevelDefinition levelDefinition)
+        {
+            initialized = false;
+        }
+    }
+}
